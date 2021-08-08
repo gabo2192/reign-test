ca7f28b43b9615ba21fb046eb9c608ec1bd852c6
Gabriel Rojas <gabriel.rojasa21@gmail.com>
2021-08-08 12:35:11 -0500

    feat: add log to what port the server is using

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = 4000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(4000);
+  await app.listen(port);
+  console.log(`Server is running on port: ${4000} `);
 }
 bootstrap();
